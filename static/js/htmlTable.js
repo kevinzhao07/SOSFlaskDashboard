f09369c1a1a027e0191d25fb8b165241ce26ef08
Stephanie <46686745+choisteph@users.noreply.github.com>
2019-07-02 16:51:56 -0400

    no latlong, timeformat

--- static/js/htmlTable.js
+++ static/js/htmlTable.js
@@ -4,21 +4,31 @@ function makeHtmlTable() {
     table = d3.select("#sosTable")
     thead = table.append('thead').append('tr')
       .selectAll('th')
-      .data(DATA.columns)
+      .data(d => DATA.columns.slice(0, -2))
       .join('th')
-        .text(d => d);
+        .text((d,i) => i = 1 ? formatHTMLthings(d): d);
     tbody = table.append('tbody');
-  
+    console.log(DATA.columns);
     updateAll(date.top(10));
   }
-  
+
 // updates HTML table
 function updateHtmlTable(data = date.top(10)) {
     rows = tbody.selectAll('tr')
         .data(data)
         .join('tr')
     rows.selectAll('td')
-        .data((d) => d3.values(d))
+        .data((d) => d3.values(d).slice(0,-2))
         .join('td')
-        .text((d,i) => i > 0 ? d : d)
+        .text((d,i) => i = 1 ? formatHTMLthings(d) : d)
+}
+
+function formatHTMLthings(d){
+  if (d instanceof Date){
+    var formatted = d3.timeFormat("%b %d %Y")(d)
+    return formatted;
+  }
+  else{
+    return d;
+  }
 }
